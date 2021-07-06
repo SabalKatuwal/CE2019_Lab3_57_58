e34d8078b1cd5e37254b373ba6022eedc061e424
Anurag Timilsina <anurag.timilsina@gmail.com>
2021-07-06 09:48:50 +0545

    project initiated!

---
+++ lib/interface.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class AbstractBST {
+    public:
+        virtual bool isEmpty() = 0;
+        virtual void add(int key, int value) = 0;
+        virtual void max(int &output) = 0;
+        virtual void min(int &output) = 0;
+        virtual bool exists(int targetKey) = 0;
+        virtual void inorder() = 0;
+}
