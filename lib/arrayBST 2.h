df752f3bd434acdfd36abfa81a35b1d0a6746e8c
SabalKatuwal <sabal.katuwal1@gmail.com>
2021-07-22 10:18:09 +0545

    merged main

---
+++ lib/arrayBST 2.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "interface.h"
+#define MAX_NODE 50
+
+
+class ArrayNode
+{
+    int key;
+    int value;
+    ArrayNode(int key, int value):key(key), value(value) {}
+};
+
+class BSTusingArray:public AbstractBST
+{
+    private:
+    ArrayNode* nodes[MAX_NODE];
+
+    public:
+    BSTusingArray();
+    ~BSTusingArray();
+    bool isEmpty();
+    void add(int key, int value);
+    void max(int &output);
+    void min(int &output);
+    void remove(int keyToDelete);
+    //void remove();
+};
